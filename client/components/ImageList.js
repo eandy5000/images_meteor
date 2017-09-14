d3d404090f2639e2df400df5d258aaeee96b83cd
Andy Eccleston <eandy5000@yahoo.com>
2017-09-14 09:34:03 -0500

    added react and imgur api to get images and rendering them to dom withing meteor framework

--- client/components/ImageList.js
+++ client/components/ImageList.js
@@ -1,13 +1,52 @@
 import React, {Component} from 'react'
+import axios from 'axios'
 
 class ImageList extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state={
+            images: []
+        }
+    }
+
+    renderList() {
+        const {images} = this.state
+        const filteredImages = images.filter(item => item.is_album === false)
+
+        return filteredImages.map(item => {
+            return (
+                <div key={item.id}>
+                    {item.title}<br/>
+                    <img src={item.link} width="300" />
+                </div>)
+        })
+    }
+
+    renderLoading() {
+        return (<div>loading...</div>)
+    }
+
     render() {
+        console.log(this.state.images)
+        if(this.state.images.length === 0) {
+           return this.renderLoading()
+        }
         return (
             <div>
-                <h6>Image List</h6>
+                {this.renderList()}
             </div>
         )
+
     }
+
+    componentWillMount() {
+        axios.get("https://api.imgur.com/3/gallery/hot/viral/0")
+             .then(res => {
+                this.setState({images: res.data.data})
+             })
+    }
+
 }
 
 export default ImageList
